2e85e56fa5a0ae925d0c6f9cd97c5b004e6789ec
Karthik Ragulan <karthik070904@kgpian.iitkgp.ac.in>
2026-08-22 01:43:57 +0530

    addition of docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-RUN_PORT=${PORT:- 8080}
+ #!/bin/bash
+    RUN_PORT=${PORT:-8080}
 
-
-/usr/local/bin uvicorn app.main:app --host 0.0.0.0 --port "${RUN_PORT}"
+    /usr/local/bin/uvicorn app.main:app --host 0.0.0.0 --port "${RUN_PORT}"
